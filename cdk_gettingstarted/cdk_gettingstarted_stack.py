a8a2cd531159bb5ae6460ef37049f48125a3c82c
AnassBenzanzoun <amazonwebservicesrestart@gmail.com>
2023-12-05 15:27:22 +0100

    summary

--- cdk_gettingstarted/cdk_gettingstarted_stack.py
+++ cdk_gettingstarted/cdk_gettingstarted_stack.py
@@ -8,33 +8,35 @@ from constructs import Construct
 
 
 class CdkGettingstartedStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Create Lambda function
-        my_lambda = lambda_.Function(
-            self, "CdkGettingstartedLambda",
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            handler="handler.lambda_handler",
-            code=lambda_.Code.from_asset('./cdk_gettingstarted/lambda_functions')
-        )
-        # IAM Policy for Lambda function
+        # Create IAM Policy for Lambda function
         lambda_policy = iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=["lambda:InvokeFunction"],
-            resources=[my_lambda.function_arn]
+            resources=["*"],  # You can refine this to a specific resource
         )
-        my_lambda.add_to_role_policy(lambda_policy)
+
+        # Create Lambda function with the IAM policy
+        my_lambda = lambda_.Function(
+            self,
+            "CdkGettingstartedLambda",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            handler="handler.lambda_handler",
+            code=lambda_.Code.from_asset("./cdk_gettingstarted/lambda_functions"),
+            initial_policy=[lambda_policy],  # Assign the policy directly
+        )
 
         # Create API Gateway
-        api = apigw.RestApi(self, "widgets-api",
-                  rest_api_name="Widget Service",
-                  description="This service serves widgets.")
+        api = apigw.RestApi(
+            self,
+            "api",
+            rest_api_name="api service",
+            description="api interface for our lambda function",
+            deploy_options={"stage_name": "prod"},
+        )
 
         get_widgets_integration = apigw.LambdaIntegration(my_lambda)
 
-        api.root.add_method("GET", get_widgets_integration)   # GET /
-            
-
-        
+        api.root.add_method("GET", get_widgets_integration)  # GET /
--- cdk_gettingstarted/lambda_functions/handler.py
+++ cdk_gettingstarted/lambda_functions/handler.py
@@ -1,1 +1,14 @@
-lambda_handler = lambda *args, **kwargs: {'mock_data': 'Hello, this is mock data!'}
+def lambda_handler(event, context):
+    try:
+        # Your actual code implementation here
+        result = {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": '{"mock_data": "Hello, this is mock data!"}',
+        }
+        return result
+    except Exception as e:
+        # Log the exception for debugging purposes
+        print(f"Exception: {e}")
+        # Return a meaningful error response
+        return {"statusCode": 500, "body": "Internal Server Error"}
